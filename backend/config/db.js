8792cfcfdef8c68592f1f93d805385315e5db7d4
sky1715 <k.suraj1715@gmail.com>
2024-08-16 15:54:16 +0530

    nodemodule

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
  export const connectDB= async ()=>{
     await mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log("DB connected"))
-    .catch(()=>console.log("DB connection error"))
+    .catch((err)=>console.log("DB connection error",err))
 }
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 import validator from 'validator'
 
 
@@ -22,7 +22,7 @@ const loginUser = async (req,res) =>{
      if(!user){
         res.json({success:false,message:"user does not exist"})
      }
-     const isMatch= await bcrypt.compare(password,user.password)
+     const isMatch= await bcryptjs.compare(password,user.password)
 
      if(!isMatch){
         return res.json({success:false,message:"Invalid Credentials"})
@@ -62,8 +62,8 @@ try {
 
     }
     // hashing user password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password,salt)
    // new user
     const newUser = new userModel({
         name:name,
